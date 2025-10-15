8267b1897a3c1afa34437f9de60bd04eae3aa649
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-10-15 14:46:03 +0200

    add h1

--- asset/main.js
+++ asset/main.js
@@ -1,1 +1,19 @@
 console.log('funzia');
+
+
+/*
+Descrizione:
+Scrivere un programma che chieda all’utente:
+Il numero di chilometri da percorrere
+Età del passeggero
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+*/
+
+// Tramite form, chiedere quant km sono da paercorrere
+// età passeggero
+// 0,21 € al km
+// se minorenne, sconto 20%
+// se over 65, sconto 40%
